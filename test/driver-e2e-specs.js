f28d918c63a8747b5b3992e9899079f3ed013792
sebv <seb.vincent@gmail.com>
2015-08-20 00:36:47 +0800

    added deleteSession

--- test/driver-e2e-specs.js
+++ test/driver-e2e-specs.js
@@ -6,21 +6,27 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import 'mochawait';
 import path from 'path';
-
+import B from 'bluebird';
 
 chai.should();
 chai.use(chaiAsPromised);
 
 describe('driver', function () {
   this.timeout(120000);
-  it('should work', async () => {
+  let driver;
+  it('should start', async () => {
     let caps = {
       app: path.resolve(rootDir, 'test', 'assets', 'TestApp.zip'),
       platformName: 'iOS',
       deviceName: 'iPhone 5'
     };
-    let driver = new IosDriver();
+    driver = new IosDriver();
     await driver.createSession(caps);
+  });
+
+  it('should stop', async () => {
+    await B.delay(2000);
+    await driver.deleteSession();
   });
 });
 
