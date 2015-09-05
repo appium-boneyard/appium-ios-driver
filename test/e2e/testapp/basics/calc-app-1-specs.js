c217c2d4668e2ef57d38252de15c534b5a04b27a
sebv <seb.vincent@gmail.com>
2015-09-06 02:16:07 +0800

    adds e2e tests

---
+++ test/e2e/testapp/basics/calc-app-1-specs.js
@@ -0,0 +1,122 @@
+import setup from "../../setup-base";
+import desired from '../desired';
+import { filterVisibleUiaSelector } from '../../helpers/recipes';
+
+describe('testapp - basics - calc app 1', function () {
+  let session = setup(this, desired);
+  let driver = session.driver;
+
+  let values = null;
+  let byUIA = '-ios uiautomation';
+
+  let clearFields = async function () {
+    values = [];
+    let els = await driver.findElements('class name', 'UIATextField');
+    for (let el of els){
+      await driver.clear(el);
+    }
+    let button = await driver.findElement('class name', 'UIAButton');
+    await driver.click(button);
+  };
+
+  let populate = async function (type) {
+    values = [];
+    let els = await driver.findElements(byUIA, filterVisibleUiaSelector('.textFields();'));
+    for (let el of els){
+      let val = Math.round(Math.random() * 10);
+      values.push(val);
+      if (type === "elem") {
+        await driver.setValue(val, el);
+      } else if (type === "elem-setvalue") {
+        await driver.setValueImmediate(val, el);
+      } else if (type === "driver") {
+        await driver.click(el);
+        await driver.keys(val);
+      }
+    }
+  };
+
+  let computeAndCheck = async function () {
+    let button = await driver.findElement('class name', 'UIAButton');
+    await driver.click(button);
+    let el = await driver.findElement('class name', 'UIAStaticText');
+    let result = parseInt(await driver.getText(el), 10);
+    result.should.equal(values[0] + values[1]);
+  };
+
+  beforeEach(async () => {
+    await clearFields();
+  });
+
+  it('should fill two fields with numbers', async () => {
+    await populate("elem");
+    await computeAndCheck();
+  });
+
+  // using sendKeysToActiveElement
+  it('should fill two fields with numbers - sendKeys', async () => {
+    await populate("driver");
+    await computeAndCheck();
+  });
+
+  it('should fill two fields with numbers - setValue', async () => {
+    await populate("elem-setvalue");
+    await computeAndCheck();
+  });
+
+  it('should confirm that button is displayed', async () => {
+    let el = await driver.findElement('class name', 'UIATextField');
+    (await driver.elementDisplayed(el)).should.be.ok;
+  });
+
+  it('should confirm that the disabled button is disabled', async () => {
+    let el = await driver.findElement('name', 'DisabledButton');
+    (await driver.elementEnabled(el)).should.not.be.ok;
+  });
+
+  it('should confirm that the compute sum button is enabled', async () => {
+    let el = await driver.findElement('name', 'ComputeSumButton');
+    (await driver.elementEnabled(el)).should.be.ok;
+  });
+
+  it('should interact with alert', async () => {
+    let button = (await driver.findElements('class name', 'UIAButton'))[1];
+    await driver.click(button);
+    await driver.postAcceptAlert();
+    await driver.click(button);
+    let text = await driver.getAlertText();
+    text.should.include("Cool title");
+    text.should.include("this alert is so cool.");
+    await driver.postDismissAlert();
+  });
+
+  it('should find alert like other elements', async () => {
+    try {
+      let button = (await driver.findElements('class name', 'UIAButton'))[1];
+      await driver.click(button);
+      let alert = await driver.findElement('class name', 'UIAAlert');
+      let els = await driver.findElementsFromElement('class name', 'UIAStaticText', alert);
+      let texts = [];
+      for (let el of els){
+        texts.push(await driver.getText(el));
+      }
+      texts.should.include('Cool title');
+      texts.should.include('this alert is so cool.');
+    } finally {
+      await driver.postDismissAlert();
+    }
+  });
+
+  it('should get tag names of elements', async () => {
+    let el = await driver.findElement('class name', 'UIAButton');
+    (await driver.getName(el)).should.equal("UIAButton");
+    el = await driver.findElement('class name', 'UIAStaticText');
+    (await driver.getName(el)).should.equal('UIAStaticText');
+  });
+
+  it('should be able to get text of a button', async () => {
+    let el = await driver.findElement('class name', 'UIAButton');
+    (await driver.getText(el)).should.equal("Compute Sum");
+  });
+
+});
---
+++ test/e2e/testapp/basics/calc-app-2-specs.js
@@ -0,0 +1,74 @@
+import setup from "../../setup-base";
+import desired from '../desired';
+import B from 'bluebird';
+import { throwMatchableError } from '../../helpers/recipes';
+import path from 'path';
+import { fs } from 'appium-support';
+import _ from 'lodash';
+
+describe('testapp - basics - calc app 2', function () {
+  let session = setup(this, desired);
+  let driver = session.driver;
+
+  it('should lookup two fields by name and populate them with ' +
+      'random numbers to finally sum them up', async () => {
+    let sum = 0;
+    let lookup = async function (textFieldNum) {
+      let num = Math.round(Math.random() * 10000);
+      sum += num;
+      let el = await driver.findElement('name', 'TextField' + textFieldNum);
+      await driver.setValue(num, el);
+    };
+
+    let answer = await driver.findElement('name', 'Answer');
+    await lookup(1);
+    await lookup(2);
+    let button = await driver.findElement('name', 'ComputeSumButton');
+    await driver.click(button);
+    let res = parseInt(await driver.getText(answer),10);
+    res.should.equal(sum);
+  });
+
+  it('should receive correct error', async () => {
+    await B.resolve(driver.execute("mobile: doesn't exist"))
+      .catch(throwMatchableError)
+      .should.be.rejectedWith(/jsonwpCode: 9/);
+  });
+
+  it('should be able to get syslog log type', async () => {
+    let logTypes = await driver.getLogTypes();
+    logTypes.should.include('syslog');
+    logTypes.should.include('crashlog');
+    logTypes.should.not.include('logcat');
+  });
+
+  // TODO: Fails on sauce, investigate
+  // TODO: Fails with 8.4 or Appium 1.5, investigate cause
+  it.skip('should be able to get syslog logs @skip-ios6 @skip-ios8 @skip-ci', async () => {
+    await driver.implicitWait(4000);
+    await B.resolve(driver.findElement('name', 'SumLabelz'))
+      .catch(throwMatchableError)
+      .should.be.rejectedWith(/jsonwpCode: 7/);
+    let logs = await driver.getLog();
+    logs.length.should.be.above(0);
+    logs[0].message.should.not.include("\n");
+    logs[0].level.should.equal("ALL");
+    logs[0].timestamp.should.exist;
+  });
+
+  it('should be able to get crashlog logs @skip-ci', async () => {
+    let dir = path.resolve(process.env.HOME, "Library", "Logs", "DiagnosticReports");
+    let msg = 'boom';
+    let logs = await driver.getLog('crashlog');
+    await fs.writeFile(dir + '/myApp_' + Date.parse(new Date()) + '_rocksauce.crash', msg);
+    logs = await driver.getLog('crashlog');
+    logs.length.should.equal(1);
+    _.last(logs).message.should.not.include("\n");
+    _.last(logs).message.should.equal(msg);
+    _.last(logs).level.should.equal("ALL");
+    _.last(logs).timestamp.should.exist;
+    logs = await driver.getLog('crashlog');
+    logs.length.should.equal(0);
+  });
+
+});
