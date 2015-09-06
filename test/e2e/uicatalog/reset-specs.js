3491b9721db421b9166a248bc5174715a90822a2
sebv <seb.vincent@gmail.com>
2015-09-06 21:45:25 +0800

    test fix

--- test/e2e/uicatalog/reset-specs.js
+++ test/e2e/uicatalog/reset-specs.js
@@ -7,24 +7,25 @@ describe('uicatalog - reset @skip-ios6', function () {
   describe('app reset', function () {
     let session = setup(this, desired);
     let driver = session.driver;
+    let rawDriver = session.rawDriver;
 
     it("should be able to find elements after a soft reset", async () => {
-      driver.ready.should.be.ok;
+      rawDriver.ready.should.be.ok;
       let els = await driver.findElements('class name', 'UIATableView');
       els.should.have.length(1);
       await driver.reset();
-      driver.ready.should.be.ok;
+      rawDriver.ready.should.be.ok;
       await B.delay(1000);
       els = await driver.findElements('class name', 'UIATableView');
       els.should.have.length(1);
     });
 
     it('should successfully close an app', async () => {
-      driver.ready.should.be.ok;
+      rawDriver.ready.should.be.ok;
       await driver.closeApp();
-      driver.ready.should.not.be.ok;
+      rawDriver.ready.should.not.be.ok;
       await driver.launchApp();
-      driver.ready.should.be.ok;
+      rawDriver.ready.should.be.ok;
       let els = await driver.findElements('class name', 'UIATableView');
       els.should.have.length(1);
     });
