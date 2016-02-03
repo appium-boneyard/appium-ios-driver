2629137bbcacda32ed609f762897854fe987e857
Scott Dixon <scott@howrefreshing.com.au>
2016-02-03 11:41:16 -0800

    Initial prepareIosOpts tests - IWD

---
+++ test/utils-specs.js
@@ -0,0 +1,26 @@
+// transpile:mocha
+
+import * as utils from '../lib/utils';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('prepareIosOpts', () => {
+  it('should use instruments without delay by default', async () => {
+    let opts = {};
+    utils.prepareIosOpts(opts);
+    opts.withoutDelay.should.be.true;
+  });
+  it('should use instruments without delay if explicitly not using native instruments', async () => {
+    let opts = {nativeInstrumentsLib: false};
+    utils.prepareIosOpts(opts);
+    opts.withoutDelay.should.be.true;
+  });
+  it('should not use instruments without delay if using native intruments', async () => {
+    let opts = {nativeInstrumentsLib: true};
+    utils.prepareIosOpts(opts);
+    opts.withoutDelay.should.be.false;
+  });
+});
