45433977106a2811f45747d3ae1d349ea4bf4b90
Allen Yin <xing.yin@sap.com>
2016-04-28 12:44:15 +0800

    add e2e test for in-built calendar

---
+++ test/e2e/driver/built-in-apps-e2e-specs.js
@@ -0,0 +1,36 @@
+// transpile:mocha
+
+import _ from 'lodash';
+import env from '../helpers/env';
+import { IosDriver } from '../../../lib/driver';
+import { rootDir } from '../../../lib/utils';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import path from 'path';
+import B from 'bluebird';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('built-in apps', function () {
+  this.timeout(120000);
+  let driver;
+  describe('Calendar', () => {
+    it('should start', async () => {
+      let caps = {
+        app: 'Calendar',
+        platformName: 'iOS',
+        deviceName: 'iPhone 6s',
+        showIOSLog: false,
+        fullReset: false,
+        newCommandTimeout: 120
+      };
+      caps = _.merge({}, env.CAPS, caps);
+      driver = new IosDriver();
+      await driver.createSession(caps);
+
+      let src = await driver.getPageSource();
+      src.should.include('UIAApplication name="Calendar" label="Calendar"');
+    });
+  });
+});
