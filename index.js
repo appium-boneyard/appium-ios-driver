92d4aa354c1c8ac690ae641bd5dbb7891b10060a
Isaac Murchie <isaac@saucelabs.com>
2016-06-28 09:08:35 -0700

    Export desired cap constraints for use in other drivers

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import yargs from 'yargs';
 import { asyncify } from 'asyncbox';
 import { startServer } from './lib/server';
 import { IosDriver } from './lib/driver';
+import desiredCapConstraints from './lib/desired-caps';
 
 const DEFAULT_HOST = "localhost";
 const DEFAULT_PORT = 4723;
@@ -19,6 +20,6 @@ if (require.main === module) {
   asyncify(main);
 }
 
-export { IosDriver };
+export { IosDriver, desiredCapConstraints };
 
 export default IosDriver;
