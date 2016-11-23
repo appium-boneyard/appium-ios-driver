784f76761456dccb95d70eb7103727071d7f48db
Dan Graham <dan@saucelabs.com>
2016-11-23 15:16:32 -0800

    Accepts customSSLCert as a capability
    -Updated driver.js to installSSLCert when it's using an IOS Simulator and customSSLCert was provided.
    -Updated appium-ios-simulator to 1.9.0 which has installSSLCert and uninstallSSLCert methods
    -Wrote a spec that sets up a temporary https site with a random PEM certificate to test that it can open said site
    -Make customSSLCert part of desired caps

---
+++ test/e2e/safari/webview/ssl-specs.js
@@ -0,0 +1,34 @@
+import desired from './desired';
+import B from 'bluebird';
+import https from 'https';
+import setup from '../../setup-base';
+
+const pem = B.promisifyAll(require('pem'));
+
+describe('When accessing an HTTPS encrypted site in Safari', async function () {
+  let sslServer;
+
+  before(async () => {
+    // Create an HTTPS server with a random pem certificate
+    let privateKey = await pem.createPrivateKeyAsync();
+    let keys = await pem.createCertificateAsync({days:1, selfSigned: true, serviceKey: privateKey.key});
+    let pemCertificate = keys.certificate;
+
+    sslServer = https.createServer({key: keys.serviceKey, cert: pemCertificate}, function (req, res) {
+      res.end('Arbitrary text');
+    }).listen(9758);
+    desired.customSSLCert = pemCertificate;
+  });
+
+  const driver = setup(this, desired, {noReset: true}).driver;
+
+  after(async () => {
+    await sslServer.close();
+  });
+
+  it('should be able to access it as long the PEM certificate is provided as a capability', async () => {
+    await B.delay(500);
+    await driver.setUrl('https://localhost:9758');
+    (await driver.getPageSource()).should.include('Arbitrary text');
+  });
+});
