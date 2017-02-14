6c6fe4a4f3b318df62bac3b16c7d2013ebc37f81
Dan Graham <dan@saucelabs.com>
2017-02-14 13:13:36 -0800

    Removed unused code
    ...added style improvements; fixed documentation

--- test/unit/iwdp-specs.js
+++ test/unit/iwdp-specs.js
@@ -1,15 +1,10 @@
 // transpile:mocha
 
 import sinon from 'sinon';
-/*import { fs } from 'appium-support';
-import path from 'path';
-import logger from '../../lib/device-log/logger';*/
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import IWDP from '../../lib/iwdp';
 import { SubProcess } from 'teen_process';
-// import B from 'bluebird';
-
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -17,15 +12,9 @@ chai.use(chaiAsPromised);
 const { expect } = chai;
 let iwdpInstance;
 
-describe.only('ios webkit debug proxy class', () => {
+describe('ios webkit debug proxy class', () => {
   beforeEach(async () => {
     iwdpInstance = new IWDP();
-  });
-
-  afterEach(async () => {
-    try {
-      await iwdpInstance.stop();
-    } catch (ign) {}
   });
 
   it('should detect that IWDP is supported on this machine', async function () {
@@ -42,6 +31,7 @@ describe.only('ios webkit debug proxy class', () => {
     await iwdpInstance.start();
     let devices = await iwdpInstance.getDevices();
     expect(devices.length).to.equal(0);
+    await iwdpInstance.stop();
   });
 
   it('should stop trying to start IWDP server after failed attempts', async function (done) {
@@ -54,23 +44,25 @@ describe.only('ios webkit debug proxy class', () => {
     iwdpInstance.start();
   });
 
-  it('should not start IWDP server if one is already started on the same port', async function(done){
+  it('should not start IWDP server if one is already started on the same port', async function(done) {
     // Copy the IWDP process and start it
-    let process = Object.assign(iwdpInstance.process);
-    await process.start();
-    iwdpInstance.on('failure', () => {
+    let process = {...iwdpInstance.process};
+
+    await process.start(0);
+    iwdpInstance.on('failure', async () => {
       done();
     });
     iwdpInstance.start();
   });
 
-  it('should start IWDP server if one is started on a different port', async function(){
+  it('should start IWDP server if one is started on a different port', async function() {
     // Start IWDP on it's default port
     let process = new SubProcess('ios_webkit_debug_proxy');
-    await process.start();
+    process.start();
     await iwdpInstance.start();
     let devices = await iwdpInstance.getDevices();
     expect(devices.length).to.equal(0);
     process.stop();
+    await iwdpInstance.stop();
   });
 });
