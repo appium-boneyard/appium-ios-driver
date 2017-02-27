086a45a591e8ffbbec20f675f60f68fc56b3abab
Dan Graham <dan@saucelabs.com>
2017-02-27 14:45:07 -0800

    Restart IWDP server when it exits

--- test/unit/iwdp-specs.js
+++ test/unit/iwdp-specs.js
@@ -4,34 +4,48 @@ import chaiAsPromised from 'chai-as-promised';
 import IWDP from '../../lib/iwdp';
 import { SubProcess } from 'teen_process';
 import request from 'request-promise';
-
+import Promise from 'bluebird';
 chai.should();
 chai.use(chaiAsPromised);
 
 const { expect } = chai;
 let iwdpInstance;
 
-describe.only('ios webkit debug proxy class', () => {
+describe('ios webkit debug proxy class', () => {
   beforeEach(async () => {
     iwdpInstance = new IWDP();
   });
 
-  it('should detect that IWDP is supported on this machine', async function () {
+  afterEach(async () => {
+    try {
+      await iwdpInstance.stop();
+    } catch (ign) { }
+  });
+
+  it('should detect that IWDP is supported on this machine', function () {
     expect(IWDP.isSupported());
   });
 
-  it('should start ios-webkit-debug-proxy and get list of 0 or more devices', async function () {
+  it('should start IWDP and be able to access the webpage', async function () {
     await iwdpInstance.start();
-    request(iwdpInstance.endpoint).should.eventually.have.string('<html');
-    await iwdpInstance.stop();
+    await request(iwdpInstance.endpoint).should.eventually.have.string('<html'); 
   });
 
   it('should start IWDP server if one is started on a different port', async function() {
-    // Start IWDP on it's default port
-    let process = new SubProcess('ios_webkit_debug_proxy');
-    process.start();
+    let process = new SubProcess('ios_webkit_debug_proxy', ['c', 'null:12345']);
+    await process.start();
     await iwdpInstance.start();
-    request(iwdpInstance.endpoint).should.eventually.have.string('<html');
-    await iwdpInstance.stop();
+    await request(iwdpInstance.endpoint).should.eventually.have.string('<html');
+  });
+
+  it('should restart after the process is stopped abruptly', async function() {
+    await iwdpInstance.start();
+    await iwdpInstance.process.stop();
+    await Promise.delay(1000); // Give the process time to start
+    await request(iwdpInstance.endpoint).should.eventually.have.string('<html');
+  });
+
+  it('should fail after reaching max retries', async function() {
+    await iwdpInstance.start();
   });
 });
