5bb0045f57fa0f0c2239612fc78da27c94fbb3cc
Dan Graham <dan@saucelabs.com>
2017-03-02 11:08:33 -0800

    Fix unspecified error problem Mocha 3 throws

--- test/unit/iwdp-specs.js
+++ test/unit/iwdp-specs.js
@@ -4,7 +4,7 @@ import chaiAsPromised from 'chai-as-promised';
 import IWDP from '../../lib/iwdp';
 import { SubProcess } from 'teen_process';
 import request from 'request-promise';
-import Promise from 'bluebird';
+import B from 'bluebird';
 chai.should();
 chai.use(chaiAsPromised);
 
@@ -42,7 +42,7 @@ describe('ios webkit debug proxy class', function () {
   it('should still start IWDP server if one is started on a different port', async function() {
     let process = new SubProcess('ios_webkit_debug_proxy', ['--config', 'null:56789']);
     process.start();
-    await Promise.delay(500);
+    await B.delay(500);
     await request('http://localhost:56789/').should.eventually.have.string('<html');
     await iwdpInstance.start();
     await request(iwdpInstance.endpoint).should.eventually.have.string('<html');
@@ -52,31 +52,37 @@ describe('ios webkit debug proxy class', function () {
   it('should restart after the process is stopped abruptly', async function () {
     await iwdpInstance.start();
     await iwdpInstance.process.stop();
-    await new Promise((resolve) => {
-      iwdpInstance.once('start', resolve);   
+    await new B((resolve) => {
+      iwdpInstance.once('start', resolve);
     });
     await request(iwdpInstance.endpoint).should.eventually.have.string('<html');
   });
 
-  it('should fail after reaching max retries', async (done) => {
+  it('should fail after reaching max retries', async ()  => {
     await iwdpInstance.start();
     let retries = 0;
 
     // It should give up restarting after 10 failed attempts
-    iwdpInstance.on('error', () => {
-      expect(retries).to.equal(10);
-      done();
+    let errorPromise = new B((resolve) => {
+      iwdpInstance.on('error', () => {
+        expect(retries).to.equal(10);
+        resolve();
+      });
     });
 
     // Keep stopping the process after it has been started
     while (++retries <= 10) {
-      let promise = new Promise((resolve) => {
+      let promise = new B((resolve) => {
         iwdpInstance.once('start', () => {
           resolve();
         });
       });
       await iwdpInstance.process.stop();
-      await promise;
+      if (retries < 10) {
+        await promise;
+      }
     }
+
+    await errorPromise;
   });
 });
