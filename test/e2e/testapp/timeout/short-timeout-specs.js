371cfdd947d69e3ff74895ec6cb7818aec7cff04
Isaac Murchie <isaac@saucelabs.com>
2018-08-16 10:37:44 -0400

    Fix for new timeout logic

--- test/e2e/testapp/timeout/short-timeout-specs.js
+++ test/e2e/testapp/timeout/short-timeout-specs.js
@@ -3,7 +3,7 @@ import desired from '../desired';
 import B from 'bluebird';
 import { throwMatchableError } from '../../helpers/recipes';
 import env from '../../helpers/env';
-import {BaseDriver} from "appium-base-driver";
+
 
 describe('testapp - timeout', function () {
   this.timeout(env.MOCHA_INIT_TIMEOUT);
@@ -15,7 +15,7 @@ describe('testapp - timeout', function () {
     let driver = session.driver;
 
     it('should die with short command timeout', async function () {
-      await driver.timeouts({protocol: BaseDriver.DRIVER_PROTOCOL.MJSONWP, type: 'command', ms: 3000}, "1dcfe021-8fc8-49bd-8dac-e986d3091b97");
+      await driver.timeouts('command', 3000);
       await B.delay(5500);
       await B.resolve(driver.findElement('accessibility id', 'dont exist dogg'))
         .catch(throwMatchableError)
