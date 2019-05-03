a17e31763a3910de4733b0c40d41a597039f70b3
Isaac A. Murchie <isaac.appium@gmail.com>
2019-05-03 11:01:56 -0400

    Mock xcrun calls (#373)
    * Mock xcrun calls
    
    * Await prepareIosOpts

--- test/unit/utils-specs.js
+++ test/unit/utils-specs.js
@@ -1,26 +1,34 @@
-// transpile:mocha
-
 import * as utils from '../../lib/utils';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import { withMocks } from 'appium-test-support';
+import xcode from 'appium-xcode';
+
 
 chai.should();
 chai.use(chaiAsPromised);
 
-describe('prepareIosOpts', function () {
-  it('should use instruments without delay by default', function () {
-    let opts = {};
-    utils.prepareIosOpts(opts);
+describe('prepareIosOpts', withMocks({xcode}, (mocks) => {
+  beforeEach(function () {
+    mocks.xcode.expects('getMaxIOSSDK')
+      .once().returns('9.3');
+  });
+  afterEach(function () {
+    mocks.verify();
+  });
+  it('should use instruments without delay by default', async function () {
+    const opts = {};
+    await utils.prepareIosOpts(opts);
     opts.withoutDelay.should.be.true;
   });
-  it('should use instruments without delay if explicitly not using native instruments', function () {
-    let opts = {nativeInstrumentsLib: false};
-    utils.prepareIosOpts(opts);
+  it('should use instruments without delay if explicitly not using native instruments', async function () {
+    const opts = {nativeInstrumentsLib: false};
+    await utils.prepareIosOpts(opts);
     opts.withoutDelay.should.be.true;
   });
-  it('should not use instruments without delay if using native intruments', function () {
-    let opts = {nativeInstrumentsLib: true};
-    utils.prepareIosOpts(opts);
+  it('should not use instruments without delay if using native intruments', async function () {
+    const opts = {nativeInstrumentsLib: true};
+    await utils.prepareIosOpts(opts);
     opts.withoutDelay.should.be.false;
   });
-});
+}));
